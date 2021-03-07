4de58a1f0b1157fb8d47300a5a9033c09d8c5bfc
rsy <943321293@qq.com>
2021-03-07 18:02:11 +0800

    1.删除lib

--- Smartlib_374/Smartlib_374/copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "pabuf=D:\VM_Ubuntu_Share\ziyansanqi\SmartDoor\GatewayHost"
-set "tobuf=C:\Users\dell\Desktop\Gateway_workplace\GatewayHost"
-copy /y "%pabuf%" "%tobuf%"
-set /p=¸´ÖÆÍê³É!<nul@pause
